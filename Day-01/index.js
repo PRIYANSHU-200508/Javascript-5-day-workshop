1b11f6a489773bb3d58d736750b8a4819a650cab
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-01-26 23:53:25 +0530

    Day 01 in js

---
+++ Day-01/index.js
@@ -0,0 +1,19 @@
+let nam  = "Priyanshu";
+
+document.writeln("My name is : " + nam  + " <br>");
+document.writeln("My name is : " + nam  + " <br>");
+document.writeln("My name is : " + nam  + " <br>"," <br>"); 
+
+
+/*
+assignment no. 01 :
+create a simple interest calculator
+*/
+ 
+
+let num1 = 12;
+let num2 = 1;
+
+document.writeln("Sum is : " + (num1 + num2) + " <br>");
+document.writeln("subtraction is : " + (num1-num2) + " <br>");
+document.writeln("multiply is : " + (num1 * num2) + " <br>"); 
