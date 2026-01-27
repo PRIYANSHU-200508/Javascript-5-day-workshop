db53b6eb18393796e412469bb576301909e33817
Priyanshu Kumar <priyanshukum1368@gmail.com>
2026-01-27 15:55:40 +0530

    Day 02 in js

---
+++ Day-02/index.js
@@ -0,0 +1,67 @@
+/*  ARRAY
+
+let arr = ["happy", "priyanshu", "priay", "priyam"]
+
+console.log("the array is: ",arr)
+console.log("the array1 is: ",arr[0])
+console.log("the array2 is: ",arr[1])
+console.log("the array3 is: ",arr[2])
+console.log("the array4 is: ",arr[3]) 
+*/
+
+
+/*
+//  OBJECT & FUNCTION
+let student = {
+    english : 90,
+    math: 30,
+    science: 49 
+}
+
+function total(Object){
+    let totalmarks = (Object.english + Object.math + Object.science)
+    console.log(totalmarks)
+}
+
+console.log(total(student))
+ */
+
+
+/*
+// FUNCTION 
+function nam(hi){
+    console.log("my name is: " + hi)
+}
+
+nam("priyanshu")
+nam("aysuh")
+nam("ram")
+nam("bittu")
+nam("ritesh")
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
